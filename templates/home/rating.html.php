94dc2ebced644d8fbed972ea05ac4d9c3b32b33b
BARTOLOMUCCI Antony <bartolomucci.developer@gmail.com>
2022-04-27 10:46:05 +0200

    added footer

---
+++ templates/home/rating.html.php
@@ -0,0 +1,3 @@
+<h1>Ils nous ont fait confiance</h1>
+
+<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
